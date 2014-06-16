0899f517328a1f1f6c6fe1551537108db97231d9
Ben Langfeld <ben@langfeld.me>
2014-06-16 16:57:48 -0300

    More specific cookbook description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "freeswitch-rayo"
 maintainer       "Mojo Lingo LLC"
 maintainer_email "ops@mojolingo.com"
 license          "Apache 2.0"
-description      "Installs/Configures FreeSWITCH"
+description      "Installs/Configures FreeSWITCH as a Rayo server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
